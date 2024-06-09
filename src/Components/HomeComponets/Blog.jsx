e09dd3635a259d860a762264829aa8aa64b39773
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-09 18:49:17 +0900

    blog section is on progress

---
+++ src/Components/HomeComponets/Blog.jsx
@@ -0,0 +1,62 @@
+const Blog = () => {
+
+    const blogs = [
+        {
+            'title': 'FROM DRIVE-THRUS TO DELIGHTS: FAST FOOD STORIES',
+            'description': 'Ac haca ullamcorper donec ante habi tasse donec imperdiet eturpis varius per a augue magna hac. Nec hac et vestibulum dui...',
+            'type': 'BUSINESS',
+            'date': 'OCTOBER 2023',
+            'image': 'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/blog_8-820x620.jpg'
+        },
+        {
+            'title': 'ON-THE-GO GOODNESS: FAST FOOD FEASTS',
+            'description': 'Ac haca ullamcorper donec ante habi tasse donec imperdiet eturpis varius per a augue magna hac. Nec hac et vestibulum dui...',
+            'type': 'CREATIVITY',
+            'date': 'OCTOBER 2023',
+            'image': 'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/blog_7-1000x720.jpg'
+        },
+        {
+            'title': 'FLAVORS IN A FLASH: EXPLORING FAST FOOD',
+            'description': 'Ac haca ullamcorper donec ante habi tasse donec imperdiet eturpis varius per a augue magna hac. Nec hac et vestibulum dui...',
+            'type': 'LIFE LESSONS',
+            'date': 'OCTOBER 2023',
+            'image': 'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/blog_6-1000x720.jpg'
+        },
+        {
+            'title': 'FAST FOOD ADVENTURES: A CULINARY RUSH',
+            'description': 'Ac haca ullamcorper donec ante habi tasse donec imperdiet eturpis varius per a augue magna hac. Nec hac et vestibulum dui...',
+            'type': 'STARTUP',
+            'date': 'OCTOBER 2023',
+            'image': 'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/blog_5-1000x720.jpg'
+        },
+        {
+            'title': 'BITE-SIZED BLISS: THE WORLD OF FAST FOOD',
+            'description': 'Ac haca ullamcorper donec ante habi tasse donec imperdiet eturpis varius per a augue magna hac. Nec hac et vestibulum dui...',
+            'type': 'BUSINESS',
+            'date': 'OCTOBER 2023',
+            'image': 'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/blog_12-1000x720.jpg'
+        },
+        {
+            'title': 'FAST FOOD FRENZY: A TASTE OF CONVENIENCE',
+            'description': 'Ac haca ullamcorper donec ante habi tasse donec imperdiet eturpis varius per a augue magna hac. Nec hac et vestibulum dui...',
+            'type': 'STARTUP',
+            'date': 'OCTOBER 2023',
+            'image': 'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/blog_10-1000x720.jpg'
+        },
+        {
+            'title': 'SAVORING SPEED: FAST FOOD FAVORITES',
+            'description': 'Ac haca ullamcorper donec ante habi tasse donec imperdiet eturpis varius per a augue magna hac. Nec hac et vestibulum dui...',
+            'type': 'LIFE LESSONS',
+            'date': 'OCTOBER 2023',
+            'image': 'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/blog_9-1000x720.jpg'
+        },
+    ]
+
+    return(
+        <div className="w-full h-12 bg-red-500">
+            {blogs.length}
+        </div>
+    )
+}
+
+export default Blog
