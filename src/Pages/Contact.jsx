bcb61747cb12a3dcf36161499c37a155a4b67726
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-21 23:59:30 +0600

    contact page structure ready

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,16 @@
+import ContactForm from "../Components/ContactComponents/ContactForm"
+import ContactInfo from "../Components/ContactComponents/ContactInfo"
+import Subscribe from "../Components/HomeComponets/Subscribe"
 import PageTop from "../Components/PageTop"
 
 
 const Contact = () => {
   return (
       <div>
-        <PageTop pageName={'CONTACT US'}/>      
+      <PageTop pageName={'CONTACT US'} />    
+      <ContactInfo />
+      <ContactForm/>
+      <Subscribe/>
     </div>
   )
 }
