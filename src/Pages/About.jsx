232a9b60e5f0bf083519fe59172a68ba5a21906d
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-22 18:01:22 +0600

    components shown on about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,16 @@
+import AboutFood from "../Components/AboutComponents/AboutFood"
+import FoodPromiseCard from "../Components/AboutComponents/FoodPromiseCard"
+import Subscribe from "../Components/HomeComponets/Subscribe"
 import PageTop from "../Components/PageTop"
 
 
 const About = () => {
   return (
-      <div>
-          <PageTop pageName={'ABOUT US'}/>
+      <div className="bg-[#F4F1EA]">
+      <PageTop pageName={'ABOUT US'} />
+      <AboutFood />
+      <FoodPromiseCard />
+      <Subscribe/>
     </div>
   )
 }
