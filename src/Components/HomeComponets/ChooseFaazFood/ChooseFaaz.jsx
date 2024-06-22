3fc8eac6c151eaad6f1ff238bcedf7b50942cd81
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-22 22:51:13 +0600

    some style added

--- src/Components/HomeComponets/ChooseFaazFood/ChooseFaaz.jsx
+++ src/Components/HomeComponets/ChooseFaazFood/ChooseFaaz.jsx
@@ -5,15 +5,15 @@ const ChooseFaaz = () => {
   return (
     <div className="max-w-screen-2xl mx-auto py-20">
       <div className="flex flex-col gap-6 justify-center text-center">
-        <p className="text-red-700 text-md font-semibold">WHY CHOOSE FASFOOD?</p>
-        <h2 className="text-slate-800 text-4xl md:text-5xl 2xl:text-6xl font-bold">BURGERS MADE WITH <br /> LOVE AND CARE</h2>
+        <p className="text-red-700 text-xl font-semibold">WHY CHOOSE FASFOOD?</p>
+        <h2 className="text-slate-800 text-4xl md:text-5xl 2xl:text-7xl font-bold">BURGERS MADE WITH <br /> LOVE AND CARE</h2>
       </div>
 
-        <div className="flex md:flex-row gap-4 flex-col px-6 lg:px-8 2xl:px-16 pt-16">
+        <div className="flex md:flex-row gap-4 flex-col px-6 lg:px-8 2xl:px-16 pt-12">
           <div>
             <ChooseFaazCard image={'https://demo2.wpopal.com/fazfood/wp-content/uploads/2024/04/h2_img-5.jpeg'} title={'OUR BURGERS'} description={'At Faazfood , we only use quality ingredients and treat them with care! We flame grill our steaks, and by using open kitchens we ensure that guests can always follow the process.'}/>
           </div>
-          <div className="mt-16">
+          <div className="mt-20">
             <ChooseFaazCard image={'https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/11/h2_img-6.jpg'} title={'YOUR OPINION IS IMPORTANT'} description={'We take all constructive feedback seriously, and we would love along with our staff – to spend every single day making Fasfood even better! Send your feedback on your experience with us here.'}/>
           </div>
           <div>
