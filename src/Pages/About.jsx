cc1795ed7ca31586bf4ee80d00652d1194380fdc
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-22 22:52:54 +0600

    components are added to page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,6 @@
 import AboutFood from "../Components/AboutComponents/AboutFood"
 import FoodPromiseCard from "../Components/AboutComponents/FoodPromiseCard"
+import NewTest from "../Components/AboutComponents/NewTest/NewTest"
 import Subscribe from "../Components/HomeComponets/Subscribe"
 import PageTop from "../Components/PageTop"
 
@@ -10,6 +11,7 @@ const About = () => {
       <PageTop pageName={'ABOUT US'} />
       <AboutFood />
       <FoodPromiseCard />
+      <NewTest/>
       <Subscribe/>
     </div>
   )
