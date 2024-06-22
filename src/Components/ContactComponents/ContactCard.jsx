ac6c895300d3848909e1f7550bf457ce61cc3954
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-22 22:50:55 +0600

    contact page all components style added

--- src/Components/ContactComponents/ContactCard.jsx
+++ src/Components/ContactComponents/ContactCard.jsx
@@ -9,10 +9,10 @@ const ContactCard = ({icon, title, info, mInfo, detail, style}) => {
               </div>
           </div>
           <div className="text-center mt-6">
-              <h3 className="text-xl text-black font-semibold mb-4">{title}</h3>
-              <p className="text-md font-light">{info}</p>
-              <p className="text-md font-light">{mInfo}</p>
-              <p className={style ? style : 'text-md font-light'}>{detail}</p>
+              <h3 className="text-2xl text-black font-bold mb-4">{title}</h3>
+              <p className="text-lg font-normal">{info}</p>
+              <p className="text-lg font-normal">{mInfo}</p>
+              <p className={style ? style : 'text-lg font-normal'}>{detail}</p>
           </div>
     </div>
   )
--- src/Components/ContactComponents/ContactForm.jsx
+++ src/Components/ContactComponents/ContactForm.jsx
@@ -1,9 +1,10 @@
+import { HiOutlineArrowRight } from "react-icons/hi"
 
 
 const ContactForm = () => {
   return (
       <div className="max-w-screen-xl mx-auto py-12">
-          <h2 className="text-center text-4xl sm:text-5xl lg:text-[3.5rem] font-bold text-slate-800 mb-8">SEND A MESSAGE</h2>
+          <h2 className="text-center text-4xl sm:text-5xl lg:text-7xl font-bold text-slate-800 mb-8">SEND A MESSAGE</h2>
           <form className="w-full lg:w-[80%] mx-auto px-4">
               <div className="w-full flex flex-col gap-4">
                 <div className="w-full flex sm:flex-row flex-col gap-4">
@@ -12,7 +13,10 @@ const ContactForm = () => {
                 </div>
                 <input className="w-full border-2 border-stone-400 rounded-xl p-4 bg-transparent" type="text" placeholder="Subject"/>
                 <textarea className="w-full border-2 border-stone-400 rounded-xl p-4 bg-transparent " rows={8} placeholder="Message"></textarea>
-              </div>
+        </div>
+        <div className="flex items-center justify-center mt-12">
+          <button className=" flex items-center gap-2 text-xl font-semibold py-4 px-8 rounded-xl bg-green-600 text-white">SUBMIT <span><HiOutlineArrowRight/></span></button>
+        </div>
           </form>
     </div>
   )
