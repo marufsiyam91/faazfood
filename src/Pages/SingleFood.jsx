16c188545772da95ff72ce71deb7a07fb02af0fd
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-29 13:03:38 +0600

    single meal item page added

---
+++ src/Pages/SingleFood.jsx
@@ -0,0 +1,22 @@
+import { useLoaderData } from "react-router-dom";
+import PageTop from "./../Components/PageTop";
+import MealDetail from "../Components/SingleMeal/MealDetail";
+import Subscribe from "../Components/Subscribe";
+
+const SingleFood = () => {
+  const meal = useLoaderData();
+  const {
+    data: { description, image, category, title, price, rating },
+  } = meal;
+
+  return (
+    <div className="bg-[#F4F1EA]">
+      <PageTop pageName={title} />
+      <MealDetail description={description} image={image} category={category} price={price} rating={rating} />
+      
+      <Subscribe/>
+    </div>
+  );
+};
+
+export default SingleFood;
