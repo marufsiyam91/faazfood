a0aaeffb0e4bb4ce138f42150f494021105b106b
Maruf Siyam <marufsiyam9123@gmail.com>
2024-06-30 22:37:45 +0600

    change in style

--- src/Components/PageTop.jsx
+++ src/Components/PageTop.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const PageTop = ({ pageName }) => {
   return (
     <div className="bg-[url('https://demo2.wpopal.com/fazfood/wp-content/uploads/2023/10/ct-bc.jpg')] w-full bg-cover h-[300px] flex flex-col justify-center items-center">
-      <h2 className="text-6xl font-semibold text-white mb-6">{pageName}</h2>
+      <h2 className="text-4xl sm:text-6xl font-semibold text-white mb-6 text-center">{pageName}</h2>
       <div className="flex items-center gap-4">
         <NavLink className={`text-white text-xl hover:text-green-600 transition`} to={"/"}>
           Home page
