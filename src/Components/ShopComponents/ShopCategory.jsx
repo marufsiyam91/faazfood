418a36366b51b76f9b7f46eedaa42dc331c4f1f1
Maruf Siyam <marufsiyam9123@gmail.com>
2024-07-02 23:26:57 +0600

    sidebar shop category detached from it's parent and moved in new component

--- src/Components/ShopComponents/ShopCategory.jsx
+++ src/Components/ShopComponents/ShopCategory.jsx
@@ -8,29 +8,21 @@ import { LuSandwich } from "react-icons/lu";
 import { CiBowlNoodles } from "react-icons/ci";
 import { GiTacos } from "react-icons/gi";
 
-
-
-
-
-
-
-
-
 const ShopCategory = () => {
   return (
-    <aside>
-      <input className="p-4 w-full rounded-md mb-4" type="text" placeholder="search for food"/>
-        <h2 className="text-3xl font-semibold text-slate-600 mb-6">Categories</h2>
+      <div>
+          <h2 className="text-3xl font-semibold text-slate-600 pb-4 mb-4 border-b border-stone-300">CATEGORIES</h2>
         <div className="flex flex-col gap-3">
           <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<GiHamburger />} title={'BURGER'}/></span>
           <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<CiFries />} title={'FRIES'}/></span>
           <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<MdCoffee />} title={'HOT DRINKS'}/></span>
           <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<GiFullPizza />} title={'PIZZA'}/></span>
+          <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<CiBowlNoodles />} title={'NOODLES'}/></span>
           <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<LuSalad />} title={'SALADS'}/></span>
           <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<LuSandwich />} title={'SANDWICH'}/></span>
           <span className="hover:text-red-700 transition duration-300"><CategoryButton icon={<GiTacos />} title={'TACO'}/></span>
         </div>
-    </aside>
+    </div>
   )
 }
 
